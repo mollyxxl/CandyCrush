729585603de99a5490df59980c8ac860c6814f81
Andy <1187820970@qq.com>
2016-02-21 09:48:13 +0800

    first commit-- game UI

---
+++ Assets/Scripts/Candy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Candy : MonoBehaviour {
+
+	public int rowIndex;
+	public int columnIndex;
+	public float xOffset=-3f;
+	public float yOffset=-2.5f;
+
+	public GameObject[] bgs;
+	private GameObject bg;
+
+	public int type;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	private void AddRandonBg()
+	{
+		if (bg != null) return;
+
+	    type = Random.Range (0, bgs.Length);
+		bg=	Instantiate (bgs [type]) as GameObject;
+		bg.transform.parent = this.transform;
+	}
+	public void UpdatePosition()
+	{
+		AddRandonBg ();
+		transform.position = new Vector3 (columnIndex+xOffset, rowIndex+yOffset, 0f);
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public Candy candy;
+	public int columnNum=10;
+	public int rowNum=7;
+	// Use this for initialization
+	void Start () {
+	
+		for (int rowIndex=0; rowIndex<rowNum; rowIndex++) {
+			for (int columnIndex =0; columnIndex<columnNum; columnIndex++) {
+				Candy c=Instantiate(candy) as Candy;	
+				c.transform.parent=this.transform;
+				c.columnIndex=columnIndex;
+				c.rowIndex=rowIndex;
+				c.UpdatePosition();
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
